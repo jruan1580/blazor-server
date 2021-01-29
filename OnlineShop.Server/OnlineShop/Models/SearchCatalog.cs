8390007e122c6e8a40bcb7ea0bfa8dd6593ef234
Jason Ruan <jason.ruan@lytx.com>
2021-01-29 15:55:22 -0800

    wip of catalog's search component

---
+++ OnlineShop.Server/OnlineShop/Models/SearchCatalog.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models
+{
+    public class SearchCatalog
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Enter something in to to search.")]
+        public string Value { get; set; }
+        public string SearchBy { get; set; }
+    }
+}
